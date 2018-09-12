86a34068c8c649a8649c173053fbd5511c0306eb
Kristoffer Andersson <kristoffer.andersson@svenska.gu.se>
2018-09-12 14:36:05 +0200

    refactored conversion from debuglevel string to constant

--- src/server/log.py
+++ src/server/log.py
@@ -10,9 +10,40 @@ debugmode = C.config['DEBUG']
 today = time.strftime("%Y%m%d")
 DEBUGFILE = os.path.join(debugmode['LOGDIR'], '%s-debug.txt' % today)
 
+def debug_str_to_int(s):
+    """
+    Converting string to logging level. Case-insensitive.
+    Defaults to logging level WARNING.
+
+    :param s: the string to convert
+    :returns: the corresponding logging.LEVEL if matching otherwise logging.WARNING
+    """
+    s_lower = s.lower() # s.casefold() would be correct, but lower is sufficient
+    # Setting logging.WARNING as default logging level
+    debuglevel = logging.WARNING
+
+    if s_lower == "debug":
+        debuglevel = logging.DEBUG
+    elif s_lower == "info":
+        debuglevel = logging.INFO
+    elif s_lower == "warning":
+        debuglevel = logging.WARNING
+    elif s_lower == "error":
+        debuglevel = logging.ERROR
+    elif s_lower == "critical":
+        debuglevel = logging.CRITICAL
+    else:
+        print("NOTE: Can't match debuglevel in the config file.")
+        print("NOTE: Using default level: WARNING.")
+    return debuglevel
+
 if debugmode['DEBUG_TO_STDERR']:
-    logging.basicConfig(stream=sys.stderr, level=debugmode['DEBUGLEVEL'],
-                        format=debugmode['LOGFMT'], datefmt=debugmode['DATEFMT'])
+    logging.basicConfig(
+        stream=sys.stderr,
+        level=debug_str_to_int(debugmode['DEBUGLEVEL']),
+        format=debugmode['LOGFMT'],
+        datefmt=debugmode['DATEFMT']
+    )
 else:
     # Create Logfile if it does not exist
     if not os.path.isfile(DEBUGFILE):
@@ -22,17 +53,10 @@ else:
         # Fix permissions
         os.chmod(DEBUGFILE, stat.S_IRUSR | stat.S_IRGRP | stat.S_IROTH |
                  stat.S_IWUSR | stat.S_IWGRP | stat.S_IWOTH)
-    # TODO rewrite this stupid code
-    if debugmode['DEBUGLEVEL'] == "DEBUG":
-        debuglevel = logging.DEBUG
-    if debugmode['DEBUGLEVEL'] == "INFO":
-        debuglevel = logging.INFO
-    if debugmode['DEBUGLEVEL'] == "WARNING":
-        debuglevel = logging.WARNING
-    if debugmode['DEBUGLEVEL'] == "ERROR":
-        debuglevel = logging.ERROR
-    if debugmode['DEBUGLEVEL'] == "CRITICAL":
-        debuglevel = logging.CRITICAL
 
-    logging.basicConfig(filename=DEBUGFILE, level=debuglevel,
-                        format=debugmode['LOGFMT'], datefmt=debugmode['DATEFMT'])
+    logging.basicConfig(
+        filename=DEBUGFILE,
+        level=debug_str_to_int(debugmode['DEBUGLEVEL']),
+        format=debugmode['LOGFMT'],
+        datefmt=debugmode['DATEFMT']
+    )
