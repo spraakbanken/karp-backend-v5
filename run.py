92fe415a335e308c430471824ca029c7ddeeddb1
Kristoffer Andersson <kristoffer.andersson@svenska.gu.se>
2018-09-12 14:36:37 +0200

    added TODO

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 # startup-scriptet (samma som strix har)
+# TODO move to karp-backend-sb
 from gevent import monkey
 monkey.patch_all()
 import sys
@@ -10,5 +11,5 @@ try:
     port = int(sys.argv[1])
 except (IndexError, ValueError):
     sys.exit("Usage %s <port>" % sys.argv[0])
-load_urls() 
+load_urls()
 WSGIServer(('0.0.0.0', port), app).serve_forever()
