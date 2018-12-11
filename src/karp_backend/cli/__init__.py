ce6abb530d877f45855c1a0ef17cf3b417b5a249
Kristoffer Andersson <kristoffer.andersson@gu.se>
2018-12-11 14:06:06 +0100

    rename module offline to cli. offline calls cli.cli_main()

---
+++ src/karp_backend/cli/__init__.py
@@ -0,0 +1,76 @@
+import karp_backend.offline.create_metadata as metadata
+import karp_backend.offline.upload_offline as upload
+
+    # TODO move stuff from upload_offline here, leave the real code there
+def cli_main(argc, argv):
+    if argv[1] == '--create_metadata':
+        outpath = 'config/fieldmappings.json'
+        metadata.print_all(outpath)
+
+    elif argv[1] == '--create_mode':
+        mode = argv[2]
+        suffix = argv[3]
+        upload.create_mode(mode, suffix)
+        print('Upload successful')
+
+    elif argv[1] == '--create_empty_index':
+        mode = argv[2]
+        suffix = argv[3]
+        upload.create_empty_index(mode, suffix)
+        print('Index created successfully')
+
+    elif argv[1] == '--import_mode':
+        mode = argv[2]
+        suffix = argv[3]
+        upload.create_mode(mode, suffix, with_id=True)
+        print('Upload successful')
+
+    elif argv[1] == '--publish_mode':
+        group = argv[2]
+        suffix = argv[3]
+        upload.publish_group(group, suffix)
+        print('Upload successful')
+
+    elif argv[1] == '--reindex_alias':
+        print('reindex')
+        index = argv[2]
+        target_suffix = argv[3]
+        upload.reindex_alias(index, target_suffix)
+
+    elif argv[1] == '--getmapping':
+        import karp_backend.offline.getmapping as gm
+        alias = argv[2]
+        outfile = argv[3]
+        gm.getmapping(alias, outfile)
+
+    elif argv[1] == '--internalize_lexicon':
+            # adds a lexicon to sql from es
+            # can be done at any time, not noticable
+            # to the end user
+            mode = argv[2]
+            to_upload = argv[3:]
+            upload.internalize_lexicon(mode, to_upload)
+            print('Upload successful')
+
+    elif argv[1] == '--printlatestversion':
+        lexicon = argv[2]
+        upload.printlatestversion(lexicon)
+
+    elif argv[1] == '--exportlatestversion':
+        lexicon = argv[2]
+        upload.printlatestversion(lexicon, debug=False, with_id=True)
+
+    # Commented since dangerous!
+    # if argv[1] == '--delete_all':
+    #    upload.delete_all()
+    #    print('Deletion successful')
+    #
+    #
+    # # Commented since dangerous!
+    # if argv[1] == '--delete_mode':
+    #    mode = argv[2]
+    #    upload.delete_mode(mode)
+    #    print('Deletion successful')
+
+    else:
+        print("Don't know what to do")
