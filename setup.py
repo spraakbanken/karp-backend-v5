e5c8b7d007803138bf52404b6d7ba5b0cf31dac6
Kristoffer Andersson <kristoffer.andersson@gu.se>
2018-12-11 14:07:52 +0100

    bump version to 5.4.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name='karp-backend',
-    version='5.4.0',
+    version='5.4.1',
     description='Backend for Karp',
     author='Språkbanken at the University of Gothenburg',
     maintainer='Språkbanken',
