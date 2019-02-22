3692e3aaaebc6a7df6df6670233ddd2f10d433db
Kristoffer Andersson <6058164+kod-kristoff@users.noreply.github.com>
2019-02-22 11:16:20 +0100

    add dotenv call

--- run.py
+++ run.py
@@ -4,6 +4,10 @@ from gevent import monkey
 monkey.patch_all()
 import sys
 import json
+
+import dotenv
+dotenv.load_dotenv(dotenv_path='.env', verbose=True)
+
 from karp5.main import app, load_urls
 from gevent.pywsgi import WSGIServer
 
