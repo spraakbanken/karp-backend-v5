5a3ce976837c80521e6f5cdd93b6e6076b7ae5c8
Kristoffer Andersson <kode.kristoff@gmail.com>
2019-03-30 21:34:58 +0100

    rewrite

--- src/karp5/__init__.py
+++ src/karp5/__init__.py
@@ -1,7 +1,7 @@
 # import this to start up the logging, always have this line at the top of the file
 import server.log
 import pkg_resources
-
+import logging
 import six
 
 from .instance_info import get_instance_path
@@ -10,14 +10,17 @@ from .instance_info import get_instance_path
 
 from flask import Flask
 
+from .config import Config
+
 
 __version__ = '5.7.1'
 __name = 'karp5'
 
 
-def create_app():
+def create_app(config_class = Config):
     app = Flask(__name, instance_path=get_instance_path())
-
+    app.config.from_object(config_class)
+    
     return app
 
 
