35c368bdbe19a6d29e84498869f4336d9ce6478f
Kristoffer Andersson <6058164+kod-kristoff@users.noreply.github.com>
2019-04-01 13:23:00 +0200

    cli stub

--- cli.py
+++ cli.py
@@ -1,5 +1,3 @@
-import sys
-
 import dotenv
 
 dotenv.load_dotenv(dotenv_path=".env", verbose=True)
@@ -7,4 +5,5 @@ dotenv.load_dotenv(dotenv_path=".env", verbose=True)
 from karp5 import cli
 
 if __name__ == "__main__":
-    cli.cli_main(len(sys.argv), sys.argv)
+    cli = cli.create_cli()
+    cli()
