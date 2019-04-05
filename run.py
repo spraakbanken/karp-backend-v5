91c02dd2109ad03e11886d853dc6e838fdd17fe1
Kristoffer Andersson <kode.kristoff@gmail.com>
2019-04-05 16:55:42 +0200

    update

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 # startup-scriptet (samma som strix har)
 # TODO move to karp-backend-sb
-from gevent import monkey
-monkey.patch_all()
+#from gevent import monkey
+#monkey.patch_all()
 import sys
 import json
 
@@ -9,11 +9,20 @@ import dotenv
 dotenv.load_dotenv(dotenv_path='.env', verbose=True)
 
 import karp5
-from gevent.pywsgi import WSGIServer
+#from gevent.pywsgi import WSGIServer
 
-try:
-    port = int(sys.argv[1])
-except (IndexError, ValueError):
-    sys.exit("Usage %s <port>" % sys.argv[0])
-app = karp5.create_app()
-WSGIServer(('0.0.0.0', port), app).serve_forever()
+def main_wsgi():
+    try:
+        port = int(sys.argv[1])
+    except (IndexError, ValueError):
+        sys.exit("Usage %s <port>" % sys.argv[0])
+    app = karp5.create_app()
+    WSGIServer(('0.0.0.0', port), app).serve_forever()
+
+def main_debug():
+    app = karp5.create_app()
+    app.run()
+
+if __name__ == '__main__':
+    main_debug()
+    
