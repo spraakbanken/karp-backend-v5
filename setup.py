b43b7a7541c369bc002adbc03e642adc15a2e2c7
Kristoffer Andersson <6058164+kod-kristoff@users.noreply.github.com>
2019-04-30 12:53:25 +0200

    Bump version to 5.10.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name='karp5',
-    version='5.10.0',
+    version='5.10.1',
     description='Backend for Karp',
     author='Språkbanken at the University of Gothenburg',
     maintainer='Språkbanken',
--- src/karp5/__init__.py
+++ src/karp5/__init__.py
@@ -15,7 +15,7 @@ from flask import Flask, request
 from karp5.config import Config, mgr as conf_mgr
 
 
-__version__ = '5.10.0'
+__version__ = '5.10.1'
 __name = 'karp5'
 
 
