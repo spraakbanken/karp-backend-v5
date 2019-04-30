d13c3bab0ef996fb1dfb06f9d619e573563b5794
Kristoffer Andersson <6058164+kod-kristoff@users.noreply.github.com>
2019-04-30 12:15:43 +0200

    copy-alias

--- src/karp5/cli/upload_offline.py
+++ src/karp5/cli/upload_offline.py
@@ -420,6 +420,7 @@ def copy_alias_to_new_index(
     source_mode,
     target_mode,
     target_suffix,
+    filter_func=None,
     create_index=True,
     query=None
 ):
@@ -447,6 +448,8 @@ def copy_alias_to_new_index(
         doc['_index'] = target_index
         return doc
 
+    if filter_func:
+        source_docs = (filter_func(doc) for doc in source_docs)
     update_docs = (update_doc(doc) for doc in source_docs)
     success, failed, total = 0, 0, 0
     errors = []
