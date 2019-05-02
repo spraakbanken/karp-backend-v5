21a0c41ee0cc444ee3619c0a3737f787bebe2ead
Kristoffer Andersson <6058164+kod-kristoff@users.noreply.github.com>
2019-05-02 12:55:26 +0200

    Bump version to 5.10.3

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
     name="karp5",
-    version="5.10.2",
+    version="5.10.3",
     description="Backend for Karp",
     author="Språkbanken at the University of Gothenburg",
     maintainer="Språkbanken",
