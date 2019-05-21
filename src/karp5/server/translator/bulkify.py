b9cce5a87b49b6504d9088bb200314432b7bbdc3
Kristoffer Andersson <6058164+kod-kristoff@users.noreply.github.com>
2019-05-21 08:41:52 +0200

    minor changes

--- src/karp5/server/translator/bulkify.py
+++ src/karp5/server/translator/bulkify.py
@@ -6,71 +6,51 @@ from __future__ import unicode_literals
 import json
 
 from karp5.document import doc_to_es
+
 # from karp5.document import doc_to_sql
 
 
-_index = 'test'
-_type = 'test'
+_index = "test"
+_type = "test"
 
 
 def bulkify(data, bulk_info={}, with_id=False):
     """ parse a string with either a list or a single json object
         annotate with meta data
     """
-    index = bulk_info.get('index', _index)
-    itype = bulk_info.get('type', _type)
+    index = bulk_info.get("index", _index)
+    itype = bulk_info.get("type", _type)
     items = json.loads(data)
     result = []
     for item in items:
-        data_doc = item['_source'] if with_id else item
+        data_doc = item["_source"] if with_id else item
         # assert 'lexiconName' in data_doc, "document doesn't have the field 'lexiconName'"
-        es_doc = doc_to_es(data_doc, data_doc['lexiconName'], 'bulk')
+        es_doc = doc_to_es(data_doc, data_doc["lexiconName"], "bulk")
         doc = {
-            '_index': index,
-            '_type': itype,
-            '_source': es_doc,
+            "_index": index,
+            "_type": itype,
+            "_source": es_doc,
         }
         if with_id:
-            doc['_id'] = item['_id']
+            doc["_id"] = item["_id"]
         result.append(doc)
     return result
 
 
-def bulkify_gen(items, bulk_info, with_id=False):
-    index_ = bulk_info.get('index')
-    type_ = bulk_info.get('type')
-
-    for item in items:
-        data_doc = item['_source'] if with_id else item
-        es_doc = doc_to_es(data_doc, data_doc['lexiconName'], 'bulk')
-        doc = {
-            '_index': index_,
-            '_type': type_,
-            '_source': es_doc,
-        }
-        if with_id:
-            doc['_id'] = item['_id']
-        yield doc
-
-
-
 def bulkify_sql(data, bulk_info={}):
     """ format a dictionary of ids mapped to sql objects into a bulk insert """
-    index = bulk_info.get('index', _index)
-    itype = bulk_info.get('type', _type)
-    return [{'_index': index, '_type': itype, '_id': _id,
-             '_source': item['doc']}
-            for _id, item in list(data.items()) if item['status'] != 'removed']
+    index = bulk_info.get("index", _index)
+    itype = bulk_info.get("type", _type)
+    return [
+        {"_index": index, "_type": itype, "_id": _id, "_source": item["doc"]}
+        for _id, item in list(data.items())
+        if item["status"] != "removed"
+    ]
     result = []
     for _id, item in list(data.items()):
-        if item['status'] != 'removed':
-            data_doc = item['doc']
-            es_doc = doc_to_es(data_doc, data_doc['lexiconName'], 'bulk')
-            doc = {
-                '_index': index,
-                '_type': itype,
-                '_source': es_doc,
-                '_id': _id
-            }
+        if item["status"] != "removed":
+            data_doc = item["doc"]
+            es_doc = doc_to_es(data_doc, data_doc["lexiconName"], "bulk")
+            doc = {"_index": index, "_type": itype, "_source": es_doc, "_id": _id}
             result.append(doc)
     return result
