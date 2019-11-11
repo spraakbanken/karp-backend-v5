99ab76132cf73ebb8b399c354631ea9bd1043a94
Kristoffer Andersson <6058164+kod-kristoff@users.noreply.github.com>
2019-11-11 14:38:47 +0100

    add docstring stubs

--- src/karp5/config/errors.py
+++ src/karp5/config/errors.py
@@ -1,11 +1,12 @@
+"""[summary]
+"""
 from karp5.errors import KarpException
 
 
 class KarpConfigException(KarpException):
+    """[summary]
+    """
+
     def __init__(self, message, debug_msg=None):
         Exception.__init__(self)
-        KarpException.__init__(
-            self,
-            message,
-            debug_msg=(debug_msg or message)
-        )
+        KarpException.__init__(self, message, debug_msg=(debug_msg or message))
