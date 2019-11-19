5708baff5b906cccebcefc01e6d5968e03aaea19
Kristoffer Andersson <6058164+kod-kristoff@users.noreply.github.com>
2019-11-19 11:45:37 +0100

    correct typo

--- src/karp5/config/config.py
+++ src/karp5/config/config.py
@@ -16,14 +16,14 @@ with open(os.path.join(get_instance_path(), "config/config.json")) as fp:
     _CONFIG = json.load(fp)
 
 KARP5_DBPASS = os.environ.get("KARP5_DBPASS")
+
+
 class Config(object):
     """[summary]"""
 
     DEBUG = False
     TESTING = False
-    LOG_LEVEL = getattr(
-        logging, _CONFIG["DEBUG"]["DEBUGLEVEL"].upper(), logging.WARNING
-    )
+    LOG_LEVEL = getattr(logging, _CONFIG["DEBUG"]["DEBUGLEVEL"].upper(), logging.WARNING)
     # LOG_LEVEL = debug_str_to_int(_CONFIG['DEBUG']['DEBUGLEVEL'])
     LOG_FMT = _CONFIG["DEBUG"].get("LOGFMT")
     LOG_DIR = _CONFIG["DEBUG"].get("LOGDIR")
@@ -31,10 +31,7 @@ class Config(object):
     LOG_TO_STDERR = _CONFIG["DEBUG"].get("DEBUG_TO_STDERR")
     SECRET_KEY = _CONFIG["SETUP"]["SECRET_KEY"]
     ABSOLUTE_PATH = _CONFIG["SETUP"]["ABSOLUTE_PATH"]
-    BACKEND_URL = (
-        _CONFIG["SETUP"].get("BACKEND_URL")
-        or "https://ws.spraakbanken.gu.se/ws/karp/v5"
-    )
+    BACKEND_URL = _CONFIG["SETUP"].get("BACKEND_URL") or "https://ws.spraakbanken.gu.se/ws/karp/v5"
     SCRIPT_PATH = _CONFIG["SETUP"]["SCRIPT_PATH"]
     SENDER_EMAIL = _CONFIG["DB"]["SENDER_EMAIL"]
     SMTP_SERVER = _CONFIG["DB"].get("SMTP_SERVER")
@@ -45,16 +42,17 @@ class Config(object):
     MAX_PAGE = _CONFIG["SETUP"]["MAX_PAGE"]
     MINIENTRY_PAGE = _CONFIG["SETUP"]["MINIENTRY_PAGE"]
     SCAN_LIMIT = _CONFIG["SETUP"]["SCAN_LIMIT"]
-    ELASTICSEARCH_URL = os.environ.get(
-        "KARP5_ELASTICSEARCH_URL", "http://localhost:9200"
-    ).split(",")
+    ELASTICSEARCH_URL = os.environ.get("KARP5_ELASTICSEARCH_URL", "http://localhost:9200").split(
+        ","
+    )
     OVERRIDE_ELASTICSEARCH_URL = strtobool(
         os.environ.get("KARP5_ELASTICSEARCH_URL_OVERRIDE", "false")
     )
     TESTING = False
     DEBUG = False
     DATABASE_BASEURL = (
-        f"mysql+pymysql://{KARP5_DBPASS}/" + "{}?charset=utf8" if KARP5_DBPASS
-        else f"mysql+pymysql://{_CONFIG["DB"]["DBPASS"]}/" + "{}?charset=utf8"
+        f"mysql+pymysql://{KARP5_DBPASS}/" + "{}?charset=utf8"
+        if KARP5_DBPASS
+        else f"mysql+pymysql://{_CONFIG['DB']['DBPASS']}/" + "{}?charset=utf8"
     )
     STANDARDMODE = _CONFIG["SETUP"]["STANDARDMODE"]
