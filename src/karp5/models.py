5dbf0ebcefc718e2f51a62bab3ccc8e24c964410
Kristoffer Andersson <kod.kristoff@gmail.com>
2020-01-09 08:42:36 +0100

    core & models

---
+++ src/karp5/models.py
@@ -0,0 +1,54 @@
+from typing import List, Optional, Union
+
+
+class User:
+    pass
+
+class Lexicon:
+    pass
+
+class Mode:
+    def __init__(self,
+                 name: str,
+                 *,
+                 elastic_url: List[str] = None,
+                 sql: Union[str, bool] = None,
+                 doc_type: str,
+                 secret_fields: List[str] = None,
+                 src: Optional[str] = None,
+                 suggestionalias: str,
+                 sort_by: List[str],
+                 head_sort_field: List[str],
+                 autocomplete_field: List[str],
+                 minientry_fields: List[str],
+                 statistics_buckets: List[str],
+                 boosts: List[str],
+                 is_index: bool = True,
+                 indexalias: str = None,
+                 groups: List[str] = None,
+                ):
+        self.name = name
+        self.elastic_url = elastic_url
+        self.sql = sql
+        self.doc_type = doc_type
+        self.secret_fields = secret_fields
+        self.src = src
+        self.suggestionalias = suggestionalias
+        self.sort_by = sort_by
+        self.head_sort_field = head_sort_field
+        self.autocomplete_field = autocomplete_field
+        self.minientry_fields = minientry_fields
+        self.statistics_buckets = statistics_buckets
+        self.boosts = boosts
+        self.is_index = is_index
+        if indexalias is None:
+            self.indexalias = name
+        else:
+            self.indexalias = indexalias
+        self.groups = groups
+
+class Group:
+    pass
+
+class Entry:
+    pass
