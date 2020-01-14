01956b8ce7ec8c529c32aafc22431dee83b33d73
Kristoffer Andersson <6058164+kod-kristoff@users.noreply.github.com>
2020-01-14 10:52:05 +0100

    use refactored get_json

--- karp5/tests/api/search/test_search.py
+++ karp5/tests/api/search/test_search.py
@@ -1,13 +1,7 @@
-import json
-
 import pytest
 
 
-def get_json(client, path):
-    print("Calling '{}' ...".format(path))
-    response = client.get(path)
-    assert 200 <= response.status_code < 300
-    return json.loads(response.data.decode())
+from karp5.tests.util import get_json
 
 
 @pytest.mark.parametrize("command", ["query", "querycount", "minientry",])
