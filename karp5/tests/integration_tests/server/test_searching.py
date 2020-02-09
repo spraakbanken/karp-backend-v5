18a2724885141b6186751a49e1d334034015403d
Kristoffer Andersson <kod.kristoff@gmail.com>
2020-02-09 17:48:11 +0100

    adjust tests

--- karp5/tests/integration_tests/server/test_searching.py
+++ karp5/tests/integration_tests/server/test_searching.py
@@ -4,7 +4,7 @@ from karp5.tests.util import get_json
 
 def test_autocomplete_with_q(client_w_panacea):
     result = get_json(client_w_panacea, "autocomplete?q=sig")
-    assert result is None
+    assert result["hits"]["total"] == 0
     #https://ws.spraakbanken.gu.se/ws/karp/v5/autocomplete?multi=kasta,docka&resource=saldom&mode=external
     #https://ws.spraakbanken.gu.se/ws/karp/v5/autocomplete?q=kasus&resource=saldom,dalin,hellqvist
     #https://ws.spraakbanken.gu.se/ws/karp/v5/autocomplete?q=kasta&resource=saldom
@@ -12,4 +12,9 @@ def test_autocomplete_with_q(client_w_panacea):
 
 def test_getcontext_panacea(client_w_panacea):
     result = get_json(client_w_panacea, "getcontext/panacea")
-    assert result is None
+    center_source = {"center": None}
+    post = []
+    pre = []
+    assert result["pre"] == pre
+    assert result["center"]["_source"] == center_source
+    assert result["post"] == post
