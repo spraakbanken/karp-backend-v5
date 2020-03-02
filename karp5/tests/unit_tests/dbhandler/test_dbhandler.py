5428b6e67c7075b3f14176d6ce478623563722cd
Kristoffer Andersson <kod.kristoff@gmail.com>
2020-03-02 08:54:23 +0100

    add test for dbhandler

--- karp5/tests/unit_tests/dbhandler/test_dbhandler.py
+++ karp5/tests/unit_tests/dbhandler/test_dbhandler.py
@@ -1,7 +1,24 @@
-from karp5.dbhandler.dbhandler import SQLNull
+from unittest import mock
+
+from karp5.dbhandler.dbhandler import SQLNull, get_entries_to_keep_gen
 
 
 def test_sql_null_w_lexicon():
     sql_null = SQLNull("LEXICON")
     assert str(sql_null) == "SQLNull: No SQL db available for LEXICON"
     assert sql_null.message == "No SQL db available for LEXICON"
+
+
+def test_get_entries_to_keep_gen():
+    lexicon = "foo"
+    entries = [
+        {"id": 1, "value": "expected", "status": "added"},
+    ]
+
+    with mock.patch(
+            "karp5.dbhandler.dbhandler.dbselect_gen", return_value=entries
+        ), mock.patch(
+            "karp5.dbhandler.dbhandler.get_engine", return_value=("ENGINE", "DBENTRY"
+        )):
+        for entry in get_entries_to_keep_gen(lexicon):
+            assert entry["value"] == "expected"
