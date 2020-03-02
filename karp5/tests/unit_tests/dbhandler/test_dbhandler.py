e505322ae12e1b407085f7998e1af672effff351
Kristoffer Andersson <6058164+kod-kristoff@users.noreply.github.com>
2020-03-02 10:38:36 +0100

    expand tests for get_entries_to_keep_gen

--- karp5/tests/unit_tests/dbhandler/test_dbhandler.py
+++ karp5/tests/unit_tests/dbhandler/test_dbhandler.py
@@ -1,4 +1,5 @@
 from unittest import mock
+import typing
 
 from karp5.dbhandler.dbhandler import SQLNull, get_entries_to_keep_gen
 
@@ -9,16 +10,25 @@ def test_sql_null_w_lexicon():
     assert sql_null.message == "No SQL db available for LEXICON"
 
 
+def generate_from_list(
+    l: typing.List[typing.Dict[str, typing.Any]]
+) -> typing.Generator[typing.Dict[str, typing.Any], None, None]:
+    for obj in l:
+        yield obj
+
+
 def test_get_entries_to_keep_gen():
     lexicon = "foo"
     entries = [
-        {"id": 1, "value": "expected", "status": "added"},
+        {"id": 1, "value": "expected", "status": "updated"},
+        {"id": 1, "value": "not expected", "status": "added"},
+        {"id": 2, "value": "not expected", "status": "removed"},
+        {"id": 3, "value": "expected", "status": "added"},
+        {"id": 3, "value": "not expected", "status": "removed"},
     ]
 
     with mock.patch(
-            "karp5.dbhandler.dbhandler.dbselect_gen", return_value=entries
-        ), mock.patch(
-            "karp5.dbhandler.dbhandler.get_engine", return_value=("ENGINE", "DBENTRY"
-        )):
+        "karp5.dbhandler.dbhandler.dbselect_gen", return_value=generate_from_list(entries)
+    ), mock.patch("karp5.dbhandler.dbhandler.get_engine", return_value=("ENGINE", "DBENTRY")):
         for entry in get_entries_to_keep_gen(lexicon):
             assert entry["value"] == "expected"
